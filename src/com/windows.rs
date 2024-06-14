efc304026e263ecbfe5d12a85f36cf7483bcfb7a
sner21 <414975415@qq.com>
2024-06-14 18:48:01 +0800

    fix

--- src/com/windows.rs
+++ src/com/windows.rs
@@ -28,8 +28,8 @@ pub fn penetrable<R:Runtime>(window:Window<R>) {
     #[cfg(target_os = "windows")]
     unsafe {
         let title = window.title().unwrap();
-        let str = CString::new(title).unwrap().as_ptr();
-        let h: HWND = FindWindowA(null_mut(), str as LPCSTR);
+        let str = CString::new(title);
+        let h: HWND = FindWindowA(null_mut(), str.unwrap().as_ptr() as LPCSTR);
         let extended_style: __int32 = GetWindowLongA(h, GWL_EXSTYLE);
         let style = extended_style | WS_EX_TRANSPARENT as LONG | WS_EX_LAYERED as LONG | WS_EX_TOOLWINDOW as LONG | WS_EX_APPWINDOW as LONG;
         SetWindowLongA(h, GWL_EXSTYLE, style);
