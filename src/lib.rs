12f7e09a8473b146b20dd89eeda4748c6cd5355e
sner21 <414975415@qq.com>
2024-10-19 23:30:03 +0800

    20241019

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,18 @@
 use tauri::{
-    Invoke,
-    plugin::Plugin,
+    plugin::{Builder, TauriPlugin},
     Runtime,
-    window::Window,
 };
 mod com;
 
-pub struct PenetrablePlugin<R: Runtime> {
-    invoke_handler: Box<dyn Fn(Invoke<R>) + Send + Sync>,
+// pub struct PenetrablePlugin<R: Runtime> {
+//     invoke_handler: Box<dyn Fn(Invoke<R>) + Send + Sync>,
+// }
+pub fn init<R: Runtime>() -> TauriPlugin<R> {
+    Builder::new("penetrable")
+        .invoke_handler(tauri::generate_handler![])
+        .on_window_ready(|window| {
+            com::windows::penetrable(window);
+        })
+        .build()
 }
 
-impl<R: Runtime>PenetrablePlugin<R> {
-    pub fn init() -> Self {
-        Self {
-            invoke_handler: Box::new(tauri::generate_handler![]),
-        }
-    }
-}
-
-impl<R: Runtime> Plugin<R> for PenetrablePlugin<R> {
-    fn name(&self) -> &'static str {
-        "penetrable"
-    }
-    fn created(&mut self, window: Window<R>) {
-        com::windows::penetrable(window)
-    }
-    fn extend_api(&mut self, message: Invoke<R>) {
-        (self.invoke_handler)(message)
-    }
-}
