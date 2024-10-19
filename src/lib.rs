d6509f0a54af1f76b7d767d260fb59c09e974ba2
sner21 <414975415@qq.com>
2024-10-20 00:22:12 +0800

    update tauri 2.0

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,6 @@ use tauri::{
 };
 mod com;
 
-// pub struct PenetrablePlugin<R: Runtime> {
-//     invoke_handler: Box<dyn Fn(Invoke<R>) + Send + Sync>,
-// }
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
     Builder::new("penetrable")
         .invoke_handler(tauri::generate_handler![])
